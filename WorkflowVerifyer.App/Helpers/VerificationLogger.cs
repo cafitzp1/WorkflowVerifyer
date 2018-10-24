6bc14fbb5b94734a0d40853b639076e15bcd955c
Connor <cafitzp1@asu.edu>
2018-10-24 12:17:13 -0700

    added projects to sln file and removed old references, fixed dependencies for app, added dependencies to class library

--- WorkflowVerifyer.App/Helpers/VerificationLogger.cs
+++ WorkflowVerifyer.App/Helpers/VerificationLogger.cs
@@ -4,29 +4,32 @@ using System.Configuration;
 using Amazon.S3;
 using Amazon.S3.Transfer;
 using Amazon.Runtime.CredentialManagement;
+using WorkflowVerifyer.Core;
 
 namespace WorkflowVerifyer.App.Helpers
 {
     internal class VerificationLogger
     {
-        // private string m_LogString;
-        // private string m_ErrorEmailBody;
-        // private string m_AwsFileDir;
-        // private string m_LocalFileDir;
-        // private DateTime? m_LogDateTime;
-        // private List<String> m_ErrorEmailAttachments;
-        // private List<String> m_ErrorEmailToAddresses;
+        private static Amazon.RegionEndpoint AWS_S3_REGION = Amazon.RegionEndpoint.USWest2;
 
-        // public IntegrationLogger(string a_AwsS3Path, DateTime? a_LogDateTime, string a_LocalFileDir)
-        // {
-        //     m_LocalFileDir = a_LocalFileDir;
-        //     m_ErrorEmailBody = String.Empty;
-        //     m_LogString = String.Empty;
-        //     m_AwsFileDir = a_AwsS3Path;
-        //     m_LogDateTime = a_LogDateTime;
-        //     m_ErrorEmailAttachments = new ArrayList();
-        //     m_ErrorEmailToAddresses = new ArrayList();
-        // }
+        private string m_LogString;
+        private string m_ErrorEmailBody;
+        private string m_AwsFileDir;
+        private string m_LocalFileDir;
+        private DateTime? m_LogDateTime;
+        private List<String> m_ErrorEmailToAddresses;
+
+        public VerificationLogger(string a_AwsS3Path, DateTime? a_LogDateTime, string a_LocalFileDir)
+        {
+            m_LocalFileDir = a_LocalFileDir;
+            m_ErrorEmailBody = String.Empty;
+            m_LogString = String.Empty;
+            m_AwsFileDir = a_AwsS3Path;
+            m_LogDateTime = a_LogDateTime;
+            m_ErrorEmailToAddresses = new List<String>();
+
+            Util.Test();
+        }
     }
 }
 
--- WorkflowVerifyer.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WorkflowVerifyer.Core
-{
-    public class Class1
-    {
-    }
-}
